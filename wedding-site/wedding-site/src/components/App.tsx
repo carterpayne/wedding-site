1b9ac9ffe3e9cf7b0b6f20408fa271aed6d71473
Carter Payne <carterpayne91@gmail.com>
2018-11-18 20:16:36 -0600

    moved to create-react-app

---
+++ wedding-site/wedding-site/src/components/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './App.module.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.App}>
+        <div className={styles.background} />
+      </div>
+    );
+  }
+}
+
+export default App;
