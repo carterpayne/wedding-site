5db8cc505c079b47b7e7f377d8c38dcee8ffdd57
Carter Payne <carter.payne@trendkite.com>
2018-12-26 17:54:35 -0600

    add more for front page

--- wedding-site/wedding-site/src/components/Announcement.jsx
+++ wedding-site/wedding-site/src/components/Announcement.jsx
@@ -3,11 +3,13 @@ import styles from './Announcement.module.css';
 
 import names from '../assets/names.png';
 import date from '../assets/date.png';
+import place from '../assets/place.png';
 
 const Announcement = () => (
   <div className={styles.Announcement}>
     <img className={styles.names} src={names} alt="Carrie and Carter" />
-    <img className={styles.date} src={date} alt="June 22nd 2019 | Santa Fe, NM" />
+    <img className={styles.date} src={date} alt="June 22nd 2019" />
+    <img className={styles.place} src={place} alt="Santa Fe, NM" />
   </div>
 );
 
